787d0121a2cb75c7b405675b34dd99600d31b0ad
Acky <ackyzrxu@students.cs.ubc.ca>
2023-07-12 10:34:28 -0700

    updated main.py to be the starting point for updateDatabase

--- main.py
+++ main.py
@@ -1,24 +1,20 @@
-from NHLStatsAPI.ProcessTeams import ProcessTeams
-from NHLStatsAPI.Team import Team
-from NHLStatsAPI.Teams import Teams
+from NHLStatsAPI.ProcessGamePlays import ProcessGamePlays
+import NHLStatsAPI.ProcessDatabase as pdata
+from NHLStatsAPI.ProcessSchedule import ProcessSchedule
+from NHLStatsAPI.ProcessBoxscores import ProcessBoxscore
+from NHLStatsAPI.NHLStatsAPI import NHLStatsAPI
+import sys
 
-
-def main():
+def main(years):
     # request =  ProcessAPI(r"https://statsapi.web.nhl.com/api/v1/teams/1/roster?season=20162017")
     # response = request.getText()
     # statusCode = request.getStatusCode()
     # print(statusCode)
-
-	pt = ProcessTeams()
-	pt.requestTeams((2017,2017))
-	teams: Teams = pt.teamsDict[2017]
-	for team in teams:
-		team: Team = team
-		print(team.__dict__)
-    
+    interface = NHLStatsAPI()
+    interface.updateDatabase(years, backup=False)
 
 
 
 if __name__ == "__main__":
-    
-	main()
+    years = list(range(int(sys.argv[1]), int(sys.argv[2]) + 1))
+    main(years)
